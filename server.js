40609c229d525d8678291e280b1de4b43488465a
Sanjeev <sanjeev882861@gmail.com>
2018-01-11 03:11:43 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const hbs = require("hbs");
+var app = express();
+hbs.registerPartials(__dirname+"/views/partials");
+app.set("view engine", "hbs");
+app.use(express.static(__dirname + "/public"));
+hbs.registerHelper("getCurrentYear",()=> new Date().getFullYear());
+
+app.use((req,res,next) =>{
+    var now = new Date();
+    console.log(`${now} :: ${req.url} :: ${req.method}`)
+    next();
+});
+
+app.use((req,res,next) =>{
+    res.render("maintenance.hbs");
+});
+
+
+app.get("/", (req,res) => {
+    //res.send("Hello Server");
+    res.render("home.hbs",{
+        pageTitle:"Home Page",
+        
+        welcome:"Welcome to my website"
+    })
+});
+
+app.get("/about", (req,res) => {
+    //res.send("About Page");
+    res.render("about.hbs",{
+        pageTitle:'About Page',
+        
+    });
+});
+
+app.get("/bad", (req,res) => {
+    res.send({
+        errorMessage: "Unable to fulfill the request"
+    });
+});
+
+
+app.listen(3000);
